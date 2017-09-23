54848e5ed2d0bd5613f77c41034e6eb516c3bba4
Ma. Ayhna R. Mones <31788616+MARMones@users.noreply.github.com>
2017-09-23 15:52:01 +0800

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,3 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Bootstrap Theme Simply Me</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      font: 20px Montserrat, sans-serif;
+      line-height: 1.8;
+      color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+      background-color: #1abc9c; 
+      color: #ffffff;
+  }
+  .bg-2 { 
+      background-color: #474e5d; 
+      color: #ffffff;
+  }
+  .bg-3 { 
+      background-color: #ffffff;
+      color: #555555;
+  }
+  .bg-4 { 
+      background-color: #2f2f2f; 
+      color: #fff;
+  }
+  .container-fluid {
+      padding-top: 70px;
+      padding-bottom: 70px;
+  }
+  .navbar {
+      padding-top: 15px;
+      padding-bottom: 15px;
+      border: 0;
+      border-radius: 0;
+      margin-bottom: 0;
+      font-size: 12px;
+      letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+      color: #1abc9c !important;
+  }
+  </style>
+</head>
+<body>
+
+
+
+
+<div class="container-fluid bg-1 text-center">
+  <h3 class="margin">Who Am I?</h3>
+  <img src="profilepic.jpg" class="img-responsive img-circle margin" style="display:inline" width="350" height="350">
+  <h3>I'm an adventurer</h3>
+</div>
+
 <?php
-	echo "Welcome to profile"; // you can add html code here.
+	echo "Welcome to my profile";
 ?>
+
+
+
+
+<footer class="container-fluid bg-4 text-center">
+  <p>MARMones.github.io/graphicsdesign&phtogoraphy</p> 
+</footer>
+
+</body>
+</html>
